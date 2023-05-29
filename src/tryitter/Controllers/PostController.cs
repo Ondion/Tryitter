7c25d21e4ec26a07bb2ed9c9e43acb741768e72d
tamires <tamireshc@gmail.com>
2023-05-29 20:52:32 -0300

    feat: add routes getAllPosts, getPostById, getPostByStudentId, GetLastPostbyStudentId

--- src/tryitter/Controllers/PostController.cs
+++ src/tryitter/Controllers/PostController.cs
@@ -10,40 +10,70 @@ namespace tryitter.Controllers;
 [Route("[controller]")]
 public class PostController : ControllerBase
 {
-  private readonly PostRepository _repository;
-  public PostController(PostRepository repository)
-  {
-    _repository = repository;
-  }
+    private readonly PostRepository _repository;
+    public PostController(PostRepository repository)
+    {
+        _repository = repository;
+    }
 
-  [HttpPost]
-  [Authorize]
-  public IActionResult CreatePost(PostRequest postRequest)
-  {
-    _repository.CreatePost(postRequest);
-    return Ok();
-  }
+    [HttpPost]
+    [Authorize]
+    public IActionResult CreatePost(PostRequest postRequest)
+    {
+        _repository.CreatePost(postRequest);
+        return Ok();
+    }
 
-  [HttpGet]
-  public IActionResult GetAllPosts()
-  {
-    var posts = _repository.GetAllPosts();
-    return Ok(posts);
-  }
+    [HttpPut("{id}")]
+    [Authorize]
+    public IActionResult UpdatePost(int id, PostRequest postRequest)
+    {
+        var response = _repository.UpdatePost(id, postRequest);
+        if (response == "Not Alowed") return Unauthorized(response);
+        return Ok(response);
+    }
 
-  [HttpPut("{id}")]
-  [Authorize]
-  public IActionResult UpdatePost(int id, PostRequest postRequest)
-  {
-    var response = _repository.UpdatePost(id, postRequest);
-    if (response == "Not Alowed") return Unauthorized(response);
-    return Ok(response);
-  }
-  [HttpGet("Student/{id}")]
-  public IActionResult GetPostByStudentId(int id)
-  {
-    var posts = _repository.GetPostByStudentId(id);
-    return Ok(posts);
-  }
+    [HttpGet]
+    public IActionResult GetAllPosts()
+    {
+        var posts = _repository.GetAllPosts();
+        return Ok(posts);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetPostsById(int id)
+    {
+        var post = _repository.GetPostById(id);
+        return Ok(post);
+    }
+
+
+    [HttpGet("Student/{id}")]
+    public IActionResult GetPostByStudentId(int id)
+    {
+        var posts = _repository.GetPostByStudentId(id);
+        return Ok(posts);
+    }
+
+    [HttpGet("Last/Student/{id}")]
+    public IActionResult GetLastPostByStudentId(int id)
+    {
+        var post = _repository.GetLastPostByStudentId(id);
+        return Ok(post);
+    }
+
+    // [HttpGet("StudentName/{name}")]
+    // public IActionResult GetPostByStudentName(string name)
+    // {
+    //     var posts = _repository.GetPostByStudentName(name);
+    //     return Ok(posts);
+    // }
+
+    // [HttpGet("Last/StudentName/{name}")]
+    // public IActionResult GetLastPostByStudentName(string name)
+    // {
+    //     var post = _repository.GetLastPostByStudentName(name);
+    //     return Ok(post);
+    // }
 
 }
--- src/tryitter/Repository/PostRepository.cs
+++ src/tryitter/Repository/PostRepository.cs
@@ -5,79 +5,141 @@ using tryitter.Repository;
 
 namespace tryitter.Repository
 {
-  public class PostRepository
-  {
-    private readonly TryitterContext _context;
-    public PostRepository(TryitterContext context)
+    public class PostRepository
     {
-      _context = context;
-    }
-    public Post CreatePost(PostRequest postRequest)
-    {
-      var student = _context.Students.FirstOrDefault(x => x.Email == postRequest.StudentEmail);
-      var postCreated = new Post
-      {
-        Content = postRequest.Content,
-        Image = postRequest.Image,
-        StudentId = student.StudentId,
-        CreatAt = DateTime.Now,
-        UpdatetAt = DateTime.Now
-      };
-      _context.Posts.Add(postCreated);
-      _context.SaveChanges();
-      return postCreated;
-    }
+        private readonly TryitterContext _context;
+        public PostRepository(TryitterContext context)
+        {
+            _context = context;
+        }
+        public Post CreatePost(PostRequest postRequest)
+        {
+            var student = _context.Students.FirstOrDefault(x => x.Email == postRequest.StudentEmail);
+            var postCreated = new Post
+            {
+                Content = postRequest.Content,
+                Image = postRequest.Image,
+                StudentId = student.StudentId,
+                CreatAt = DateTime.Now,
+                UpdatetAt = DateTime.Now
+            };
+            _context.Posts.Add(postCreated);
+            _context.SaveChanges();
+            return postCreated;
+        }
+        public string UpdatePost(int id, PostRequest postRequest)
+        {
+            var oldPost = _context.Posts.AsNoTracking().Where(c => c.PostId == id).FirstOrDefault();
+            var student = _context.Students.AsNoTracking().Where(c => c.Email == postRequest.StudentEmail).FirstOrDefault();
+            if (oldPost.StudentId != student.StudentId) return "Not Alowed";
+            var post = new Post
+            {
+                PostId = id,
+                StudentId = oldPost.StudentId,
+                Content = postRequest.Content,
+                CreatAt = oldPost.CreatAt,
+                UpdatetAt = DateTime.Now,
+                Image = postRequest.Image ?? oldPost.Image,
+            };
+            _context.Posts.Update(post);
+            _context.SaveChanges();
+            return "Post updated";
+        }
 
-    // public List<Post> GetAllPosts()
-    // {
-    //   List<PostResponse> listPosts = new List<PostResponse>();
-    //   var posts = _context.Posts?.ToList();
-    //   foreach (Post post in posts)
-    //   {
-    //     PostResponse postResponse = new PostResponse
-    //     {
-    //       PostId = post.PostId,
-    //       Content = post.Content,
-    //       Image = post.Image,
-    //       CreatAt = post.CreatAt,
-    //       UpdatetAt = post.UpdatetAt,
-    //       StudentId = post.StudentId
-    //     };
-    //     listPosts.Add(postResponse);
-    //   }
-    //   return posts;
-    // }
+        public List<PostResponse> GetAllPosts()
+        {
+            List<PostResponse> listPosts = new List<PostResponse>();
+            var posts = _context.Posts.ToList();
+            foreach (Post post in posts)
+            {
+                PostResponse postResponse = new PostResponse
+                {
+                    PostId = post.PostId,
+                    Content = post.Content,
+                    Image = post.Image,
+                    CreatAt = post.CreatAt,
+                    UpdatetAt = post.UpdatetAt,
+                    StudentId = post.StudentId
+                };
+                listPosts.Add(postResponse);
+            }
+            return listPosts;
+        }
 
-    public Post GetPostById(int id)
-    {
-      var post = _context.Posts.Find(id);
-      return post;
+        public PostResponse GetPostById(int id)
+        {
+            var post = _context.Posts.Find(id);
+            PostResponse postResponse = new PostResponse
+            {
+                PostId = post.PostId,
+                Content = post.Content,
+                Image = post.Image,
+                CreatAt = post.CreatAt,
+                UpdatetAt = post.UpdatetAt,
+                StudentId = post.StudentId
+            };
+            return postResponse;
+        }
+
+        public List<PostResponse> GetPostByStudentId(int id)
+        {
+            List<PostResponse> listPosts = new List<PostResponse>();
+            var posts = _context.Posts.Where(p => p.StudentId == id).ToList();
+            foreach (Post post in posts)
+            {
+                var postResponse = new PostResponse
+                {
+                    PostId = post.PostId,
+                    Content = post.Content,
+                    CreatAt = post.CreatAt,
+                    UpdatetAt = post.UpdatetAt,
+                    Image = post.Image,
+                    StudentId = post.StudentId
+                };
+                listPosts.Add(postResponse);
+            }
+            return listPosts;
+        }
+
+        public PostResponse GetLastPostByStudentId(int id)
+        {
+            var posts = _context.Posts.Where(p => p.StudentId == id).ToList();
+            var post = posts.LastOrDefault();
+            var postResponse = new PostResponse
+            {
+                PostId = post.PostId,
+                Content = post.Content,
+                CreatAt = post.CreatAt,
+                UpdatetAt = post.UpdatetAt,
+                Image = post.Image,
+                StudentId = post.StudentId
+            };
+            return postResponse;
+        }
+        //!Continuar student name falta o delete post
+        // public List<Post> GetPostByStudentName(string name)
+        // {
+        //     var student = _context.Students.FirstOrDefault(s => s.Name == name);
+        //     var posts = _context.Posts.Where(p => p.StudentId == student.StudentId).ToList();
+        //     return posts;
+        // }
+
+        // public PostResponse GetLastPostByStudentName(string name)
+        // {
+        //     var student = _context.Students.FirstOrDefault(s => s.Name == name);
+        //     var posts = _context.Posts.Where(p => p.StudentId == student.StudentId).ToList();
+        //     var post = posts.LastOrDefault();
+        //     var postResponse = new PostResponse
+        //     {
+        //         PostId = post.PostId,
+        //         Content = post.Content,
+        //         CreatAt = post.CreatAt,
+        //         UpdatetAt = post.UpdatetAt,
+        //         Image = post.Image,
+        //         StudentId = post.StudentId
+        //     };
+        //     return postResponse;
+        // }
+
     }
-
-    public List<Post> GetPostByStudentId(int id)
-    {
-      var posts = _context.Posts.Where(p => p.StudentId == id).ToList();
-      return posts;
-    }
-
-    public string UpdatePost(int id, PostRequest postRequest)
-    {
-      var oldPost = _context.Posts.AsNoTracking().Where(c => c.PostId == id).FirstOrDefault();
-      var student = _context.Students.AsNoTracking().Where(c => c.Email == postRequest.StudentEmail).FirstOrDefault();
-      if (oldPost.StudentId != student.StudentId) return "Not Alowed";
-      var post = new Post
-      {
-        PostId = id,
-        StudentId = oldPost.StudentId,
-        Content = postRequest.Content,
-        CreatAt = oldPost.CreatAt,
-        UpdatetAt = DateTime.Now,
-        Image = postRequest.Image ?? oldPost.Image,
-      };
-      _context.Posts.Update(post);
-      _context.SaveChanges();
-      return "Post updated";
-    }
-
-  }
 }
