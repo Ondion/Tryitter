5fb5681aec2a7d56dacb00a2bdc53250613c28f7
tamireshc <tamireshc@gmail.com>
2023-06-02 16:37:24 -0300

    fix: organization  tests for route student

--- src/tryitter.Test/TestStudentController.cs
+++ src/tryitter.Test/TestStudentController.cs
@@ -2,38 +2,15 @@ using System.Net.Http.Headers;
 using System.Text;
 using tryitter.Services;
 using tryitter.Models;
-using Moq;
-using tryitter.Repository;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using tryitter.Models;
-using tryitter.Repository;
-
 
 namespace tryitter.Test;
 
 public class TestStudentController : IClassFixture<TestTryitterContext<Program>>
 {
   private readonly HttpClient _client;
-  // private readonly Mock<IStudentRepository> _repositoryMock;
-  // private readonly string token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9zaWQiOiI4IiwibmFtZSI6IkFuYSIsImVtYWlsIjoiYW5hQGdtYWlsIiwibmJmIjoxNjg1NjIyMTYzLCJleHAiOjE2ODU4ODEzNjMsImlhdCI6MTY4NTYyMjE2M30.luQMd2NH755p7fDyda64Tl4-2BLm6YLUV-5p9Z9r6iw";
-
-  // public TestTryitterContext<Program> _factory { get; }
-
   public TestStudentController(TestTryitterContext<Program> factory)
   {
     _client = factory.CreateClient();
-    // _repositoryMock = new Mock<IStudentRepository>();
-
-    // _client = factory.WithWebHostBuilder(builder =>
-    // {
-    //   builder.ConfigureServices(services =>
-    //       {
-    //         services.AddScoped<IStudentRepository>(st => _repositoryMock.Object);
-    //       });
-    // }).CreateClient();
   }
 
   [Fact]
@@ -44,9 +21,6 @@ public class TestStudentController : IClassFixture<TestTryitterContext<Program>>
     //Create Student
     var jsonToAdd = "{\"name\":\"Maria\",\"email\":\"maria@gmail.com\",\"password\":\"xft@ff\",\"status\":\"Focada\"}";
     var stringContent = new StringContent(jsonToAdd, Encoding.UTF8, "application/json");
-
-    // _repositoryMock.Setup(db => db.AddStudent(student)).Returns("student created");
-
     var result = await _client.PostAsync("/Student", stringContent);
     result.StatusCode.Should().Be((System.Net.HttpStatusCode)200);
     var stringResult = result.Content.ReadAsStringAsync().Result;
@@ -55,45 +29,46 @@ public class TestStudentController : IClassFixture<TestTryitterContext<Program>>
     var token = new TokenGenerator().Generate(student);
     _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
     await _client.DeleteAsync("/Student/5");
-    // var teste = await _client.GetAsync("/Student");
-    // var resultAddSameUserTwiceString = teste.Content.ReadAsStringAsync().Result;
-    // resultAddSameUserTwiceString.Should().Be("Email already exists");
-
   }
 
   [Fact]
   public async Task CreateStudentWithAExistingEmail()
   {
-    //Create a Student with a exiting email in DB
+    //Create a Student with a exiting email inMemory in DB
     var jsonToAdd = "{\"name\":\"Tom\",\"email\":\"tom@gmail.com\",\"password\":\"xft@ff\",\"status\":\"Focada\"}";
     var stringContent = new StringContent(jsonToAdd, Encoding.UTF8, "application/json");
     var result = await _client.PostAsync("/Student", stringContent);
     result.StatusCode.Should().Be((System.Net.HttpStatusCode)400);
     var resultString = result.Content.ReadAsStringAsync().Result;
     resultString.Should().Be("Email already exists");
-    //Reset in memoryDB
-    // _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
-    // await _client.DeleteAsync("/Student/1");
   }
 
   [Fact]
   public async Task LoginWithAExistingStudent()
   {
+    var student = new Student { Name = "Pedro", Email = "pedro@gmail.com", Password = "xft@ff", Status = "Focada" };
+
+    //Create Student
+    var jsonToAdd = "{\"name\":\"Pedro\",\"email\":\"pedro@gmail.com\",\"password\":\"xft@ff\",\"status\":\"Focada\"}";
+    var stringContent = new StringContent(jsonToAdd, Encoding.UTF8, "application/json");
+    await _client.PostAsync("/Student", stringContent);
+
     // login
-    var jsonToAddLogin = "{\"name\":\"ana\",\"password\":\"xft@ff\"}";
+    var jsonToAddLogin = "{\"email\":\"pedro@gmail.com\",\"password\":\"xft@ff\"}";
     var stringContentLogin = new StringContent(jsonToAddLogin, Encoding.UTF8, "application/json");
     var resultLogin = await _client.PostAsync("/Login", stringContentLogin);
     resultLogin.StatusCode.Should().Be((System.Net.HttpStatusCode)200);
     // Reset DB
-    // _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
-    // await _client.DeleteAsync("/Student/1");
+    var token = new TokenGenerator().Generate(student);
+    _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+    await _client.DeleteAsync("/Student/6");
   }
 
   [Fact]
   public async Task LoginWithANonExistingStudent()
   {
     //login
-    var jsonToAddLoginWithError = "{\"name\":\"Carlos\",\"password\":\"xf\"}";
+    var jsonToAddLoginWithError = "{\"email\":\"carlos@gmail\",\"password\":\"xf\"}";
     var stringContentLogin = new StringContent(jsonToAddLoginWithError, Encoding.UTF8, "application/json");
     var resultLogin = await _client.PostAsync("/Login", stringContentLogin);
     resultLogin.StatusCode.Should().Be((System.Net.HttpStatusCode)400);
@@ -104,43 +79,21 @@ public class TestStudentController : IClassFixture<TestTryitterContext<Program>>
   [Fact]
   public async Task GetAllStudents()
   {
-    //Create 2 Students
-    // var jsonToAdd = "{\"name\":\"Ana\",\"email\":\"an@gmail.com\",\"password\":\"xft@ff\",\"status\":\"Focada\"}";
-    // var stringContent = new StringContent(jsonToAdd, Encoding.UTF8, "application/json");
-    // await _client.PostAsync("/Student", stringContent);
-
-    // var jsonToAdd2 = "{\"name\":\"Paulo\",\"email\":\"paulo@gmail.com\",\"password\":\"xft@ff\",\"status\":\"Focado\"}";
-    // var stringContent2 = new StringContent(jsonToAdd2, Encoding.UTF8, "application/json");
-    // await _client.PostAsync("/Student", stringContent2);
     //login
     var getAllStudentsResult = await _client.GetAsync("/Student");
     getAllStudentsResult.StatusCode.Should().Be((System.Net.HttpStatusCode)200);
     var getAllStudentsResultString = getAllStudentsResult.Content.ReadAsStringAsync().Result;
-    getAllStudentsResultString.Should().Be("[{\"studentId\":1,\"name\":\"Ana\",\"email\":\"ana@gmail.com\",\"status\":\"Focada\"},{\"studentId\":2,\"name\":\"Paulo\",\"email\":\"paulo@gmail.com\",\"status\":\"Focada\"},{\"studentId\":3,\"name\":\"Tom\",\"email\":\"tom@gmail.com\",\"status\":\"Focada\"},{\"studentId\":4,\"name\":\"Joao\",\"email\":\"joao@gmail.com\",\"status\":\"Focada\"}]");
-    //Reset DB
-    // _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
-    // await _client.DeleteAsync("/Student/1");
-    // await _client.DeleteAsync("/Student/2");
+    getAllStudentsResultString.Should().Be("[{\"studentId\":1,\"name\":\"Aninha\",\"email\":\"ana@gmail.com\",\"status\":\"Focada\"},{\"studentId\":2,\"name\":\"Paulo\",\"email\":\"paulo@gmail.com\",\"status\":\"Focada\"},{\"studentId\":3,\"name\":\"Tom\",\"email\":\"tom@gmail.com\",\"status\":\"Focada\"},{\"studentId\":4,\"name\":\"Joao\",\"email\":\"joao@gmail.com\",\"status\":\"Focada\"}]");
   }
 
   [Fact]
   public async Task GetStudentWithACorrectId()
   {
-    //Create Student
-    // var jsonToAdd = "{\"name\":\"Maria\",\"email\":\"maria@gmail.com\",\"password\":\"xft@ff\",\"status\":\"Focada\"}";
-    // var stringContent = new StringContent(jsonToAdd, Encoding.UTF8, "application/json");
-    // var result = await _client.PostAsync("/Student", stringContent);
-    // result.StatusCode.Should().Be((System.Net.HttpStatusCode)200);
     //GetStudentById
     var resultGetStudentById = await _client.GetAsync("Student/1");
     resultGetStudentById.StatusCode.Should().Be((System.Net.HttpStatusCode)200);
     var resultGetStudentByIdString = resultGetStudentById.Content.ReadAsStringAsync().Result;
     resultGetStudentByIdString.Should().Be("{\"studentId\":1,\"name\":\"Ana\",\"email\":\"ana@gmail.com\",\"status\":\"Focada\"}");
-    //Reset DB
-    // _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
-    // await _client.DeleteAsync("/Student/1");
-
-
   }
 
   [Fact]
@@ -152,6 +105,38 @@ public class TestStudentController : IClassFixture<TestTryitterContext<Program>>
     var resultgetStudentByIdString = resultgetStudentById.Content.ReadAsStringAsync().Result;
     resultgetStudentByIdString.Should().Be("Student not found");
   }
+
+  [Fact]
+  public async Task GetStudentWithAExintingName()
+  {
+    var request = new HttpRequestMessage
+    {
+      Method = HttpMethod.Get,
+      RequestUri = new Uri("/Student/Name"),
+      Content = new StringContent("{\"name\":\"Paulo\"}", Encoding.UTF8, "application/json"),
+    };
+    var result = await _client.SendAsync(request).ConfigureAwait(false);
+    result.StatusCode.Should().Be((System.Net.HttpStatusCode)200);
+    var getStudentsResultString = result.Content.ReadAsStringAsync().Result;
+    getStudentsResultString.Should().Be("{\"studentId\":2,\"name\":\"Paulo\",\"email\":\"paulo@gmail.com\",\"status\":\"Focada\"}");
+  }
+
+  [Fact]
+  public async Task GetStudentWithANonExintingName()
+  {
+    var request = new HttpRequestMessage
+    {
+      Method = HttpMethod.Get,
+      RequestUri = new Uri("/Student/Name"),
+      Content = new StringContent("{\"name\":\"Mickey\"}", Encoding.UTF8, "application/json"),
+    };
+    var result = await _client.SendAsync(request).ConfigureAwait(false);
+    result.StatusCode.Should().Be((System.Net.HttpStatusCode)400);
+    var getStudentsResultString = result.Content.ReadAsStringAsync().Result;
+    getStudentsResultString.Should().Be("Student not found");
+  }
+
+
 
   [Fact]
   public async Task DeleteStudentWithACorrectId()
@@ -193,5 +178,44 @@ public class TestStudentController : IClassFixture<TestTryitterContext<Program>>
     resultDeleteStudent.StatusCode.Should().Be((System.Net.HttpStatusCode)401);
   }
 
-  //!falta o update e o get com nome
+  [Fact]
+  public async Task UpdateStudent()
+  {
+    var student = new Student { Name = "Ana", Email = "ana@gmail.com", Password = "xft@ff", Status = "Focada" };
+    //UpdateStudent
+    var token = new TokenGenerator().Generate(student);
+    _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+    var jsonToAdd = "{\"name\":\"Aninha\",\"email\":\"ana@gmail.com\",\"password\":\"xft@ff\",\"status\":\"Focada\"}";
+    var stringContent = new StringContent(jsonToAdd, Encoding.UTF8, "application/json");
+    var resultUpdateStudent = await _client.PutAsync("Student/1", stringContent);
+    resultUpdateStudent.StatusCode.Should().Be((System.Net.HttpStatusCode)200);
+    var resultUpdateStudentString = resultUpdateStudent.Content.ReadAsStringAsync().Result;
+    resultUpdateStudentString.Should().Be("Student updated");
+  }
+
+  [Fact]
+  public async Task UpdateStudentWithoutToken()
+  {
+    //UpdateStudent
+    var jsonToAdd = "{\"name\":\"Aninha\",\"email\":\"ana@gmail.com\",\"password\":\"xft@ff\",\"status\":\"Focada\"}";
+    var stringContent = new StringContent(jsonToAdd, Encoding.UTF8, "application/json");
+    var resultUpdateStudent = await _client.PutAsync("Student/1", stringContent);
+    resultUpdateStudent.StatusCode.Should().Be((System.Net.HttpStatusCode)401);
+  }
+
+  [Fact]
+  public async Task UpdateStudentWithAExitingEmail()
+  {
+    var student = new Student { Name = "Ana", Email = "ana@gmail.com", Password = "xft@ff", Status = "Focada" };
+    //DeleteStudent
+    var token = new TokenGenerator().Generate(student);
+    _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+    var jsonToAdd = "{\"name\":\"Aninha\",\"email\":\"tom@gmail.com\",\"password\":\"xft@ff\",\"status\":\"Focada\"}";
+    var stringContent = new StringContent(jsonToAdd, Encoding.UTF8, "application/json");
+    var resultUpdateStudent = await _client.PutAsync("Student/1", stringContent);
+    resultUpdateStudent.StatusCode.Should().Be((System.Net.HttpStatusCode)400);
+    var resultUpdateStudentString = resultUpdateStudent.Content.ReadAsStringAsync().Result;
+    resultUpdateStudentString.Should().Be("Email already exists");
+  }
+
 }
