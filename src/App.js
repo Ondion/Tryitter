268495adde11f945173672af341fcce51fa09279
Ondion <fabio.xavier@live.com>
2023-06-06 15:02:13 -0300

    feat: login page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,78 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from './Login';
+import { useState, useEffect } from 'react';
+
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  // useEffect(() => {
+  
+
+  //   if (email !== '' && password !== '') {
+  //     login();
+  //   }
+  // }, [email, password]);
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    const loginEndpoint = 'https://tryitter.azurewebsites.net/login';
+    const login = async () => {
+      try {
+        const response = await fetch(loginEndpoint, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ email, password })
+        });
+
+        if (response) {
+          const data = await response;
+          console.log('OK:', data);
+        } else {
+          console.error('Falha:', response.status);
+        }
+      } catch (error) {
+        console.error('Erro:', error);
+      }
+    };
+
+    login();
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {isAuthenticated ?
+          <div>oi</div>
+          :
+          <>
+            <form>
+              <p>Login</p>
+              <div className="mb-3">
+                <label htmlFor="exampleInputEmail1" className="form-label"></label>
+                <input onChange={handleEmailChange} type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="usuário" />
+              </div>
+              <div class="mb-3">
+                <label htmlFor="exampleInputPassword1" className="form-label"></label>
+                <input onChange={handlePasswordChange} type="password" className="form-control" id="exampleInputPassword1" placeholder="senha" />
+              </div>
+              <button onClick={handleLogin} className="btn btn-primary">Entrar</button>
+            </form>
+          </>
+        }
       </header>
     </div>
   );
